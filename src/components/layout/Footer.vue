95014c4e3edd3734df345e738caaab18111ea0ea
SSophiaStep <stepaniatova.sofiia@chnu.edu.ua>
2025-11-17 00:45:03 +0200

    Add updated Layout/UI

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -5,15 +5,15 @@
 
       <ul class="list-unstyled">
         <li>Степанятова Софія — 343-4 — 
-          <a class="text-info" href="https://github.com/ssopphiaa" target="_blank">GitHub</a>
+          <a class="text-info" href="https://github.com/SSophiaStep" target="_blank">GitHub</a>
         </li>
 
         <li>Горин Владислав - 343-2 - 
-          <a class="text-info" href="https://github.com/" target="_blank">GitHub</a>
+          <a class="text-info" href="https://github.com/VladdHoryn" target="_blank">GitHub</a>
         </li>
 
         <li>Журик Максим - 343-2 -
-          <a class="text-info" href="https://github.com/" target="_blank">GitHub</a>
+          <a class="text-info" href="https://github.com/zhurykmaks11" target="_blank">GitHub</a>
         </li>
       </ul>
     </div>
