a96b058718e47cdd247823195fa3072a6b92850f
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-17 05:13:43 +0200

    add Header and Footer to FavoritesPage

--- src/pages/FavoritesPage.vue
+++ src/pages/FavoritesPage.vue
@@ -1,6 +1,8 @@
 <template>
+  <Header />
+
   <div class="container py-4">
-    <h1 class="mb-4 text-center">Favorites</h1>
+    <h1 class="mb-4 text-center text-primary">Favorites</h1>
 
     <div v-if="favorites.length === 0" class="text-center text-muted">
       No favorites yet
@@ -8,10 +10,13 @@
 
     <FavoritesList :favorites="favorites" class="row g-3 mt-3" />
   </div>
+
+  <Footer />
 </template>
 
-
 <script setup lang="ts">
+import Header from "@/components/layout/Header.vue";
+import Footer from "@/components/layout/Footer.vue";
 import FavoritesList from "@/components/FavoritesList.vue";
 import { useFavoritesStore } from "@/store/favorites.store";
 
