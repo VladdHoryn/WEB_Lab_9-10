d3358d2e262d49fc5354c11724d77a14fe22eda9
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-17 05:06:49 +0200

    Add styles to CartItem

--- src/components/CarItem.vue
+++ src/components/CarItem.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="card mb-3 p-3">
-    <h5>{{ item.product.title }}</h5>
-    <p>{{ item.product.price }}$</p>
+  <div class="card mb-3 p-3 shadow-sm d-flex flex-column flex-md-row align-items-center justify-content-between">
+    <div class="me-3">
+      <h5>{{ item.product.title }}</h5>
+      <p class="fw-bold mb-1">{{ item.product.price }}$</p>
+    </div>
 
     <input
       type="number"
       min="1"
-      class="form-control w-25"
+      class="form-control w-25 mb-2 mb-md-0"
       v-model.number="item.quantity"
       @change="update(item.product.id, item.quantity)"
     />
 
-    <button
-      class="btn btn-danger mt-2"
-      @click="remove(item.product.id)"
-    >
+    <button class="btn btn-danger mt-2 mt-md-0" @click="remove(item.product.id)">
       Remove
     </button>
   </div>
@@ -28,7 +27,7 @@ interface CartItem {
   quantity: number;
 }
 
-const props = defineProps<{
+defineProps<{
   item: CartItem;
   update: (id: number, qty: number) => void;
   remove: (id: number) => void;
