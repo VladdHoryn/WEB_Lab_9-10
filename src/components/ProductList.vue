7cba6a2827204cba227fa225825b80bcd50d9302
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-17 05:04:07 +0200

    Add styles to ProductList

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -3,7 +3,7 @@
     <div
       v-for="p in products"
       :key="p.id"
-      class="col-md-3 mb-3"
+      class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4"
     >
       <ProductCard :product="p" />
     </div>
--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,26 +1,34 @@
 <template>
   <div class="container py-4">
-    <h1>Cart</h1>
+    <h1 class="mb-4 text-center">Cart</h1>
 
-    <div v-if="items.length === 0">Cart is empty</div>
+    <div v-if="items.length === 0" class="alert alert-info text-center">
+      Cart is empty
+    </div>
 
-    <CartItem
-      v-for="i in items"
-      :key="i.product.id"
-      :item="i"
-      :update="updateQuantity"
-      :remove="removeFromCart"
-    />
+    <div class="list-group mb-3">
+      <CartItem
+        v-for="i in items"
+        :key="i.product.id"
+        :item="i"
+        :update="updateQuantity"
+        :remove="removeFromCart"
+        class="list-group-item mb-2"
+      />
+    </div>
 
-    <button
-      v-if="items.length"
-      class="btn btn-warning mt-3"
-      @click="clearCart"
-    >
-      Clear Cart
-    </button>
+    <div class="text-center">
+      <button
+        v-if="items.length"
+        class="btn btn-warning mt-3"
+        @click="clearCart"
+      >
+        Clear Cart
+      </button>
+    </div>
   </div>
 </template>
+
 
 <script setup lang="ts">
 import CartItem from "@/components/CarItem.vue";
