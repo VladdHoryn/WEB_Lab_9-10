e89dbab2c8e0be3b76d17109850349cb454d0565
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-17 05:01:09 +0200

    Add styles to NotFoundPage Change styles to FavoritesPage

--- src/pages/FavoritesPage.vue
+++ src/pages/FavoritesPage.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="container py-4">
-    <h1>Favorites</h1>
+    <h1 class="mb-4 text-center">Favorites</h1>
 
-    <div v-if="favorites.length === 0">No favorites yet</div>
+    <div v-if="favorites.length === 0" class="text-center text-muted">
+      No favorites yet
+    </div>
 
-    <FavoritesList :favorites="favorites" />
+    <FavoritesList :favorites="favorites" class="row g-3 mt-3" />
   </div>
 </template>
+
 
 <script setup lang="ts">
 import FavoritesList from "@/components/FavoritesList.vue";
