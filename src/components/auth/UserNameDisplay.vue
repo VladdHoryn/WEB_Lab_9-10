291fdadd8692818159702aae0e1902844769f9cf
zhurykmaks11 <zhurik.max@gmail.com>
2025-11-17 22:03:25 +0200

    Rebuilt getProfile() and login() to use proper return types Fixed authStore to match updated API behavior - Restored Header.vue login/logout UI logic

---
+++ src/components/auth/UserNameDisplay.vue
@@ -0,0 +1,11 @@
+<template>
+  <span v-if="authStore.isAuth" class="ms-2">
+    👋 Hello, <strong>{{ authStore.user?.name }}</strong>
+  </span>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from "@/store/authStore";
+
+const authStore = useAuthStore();
+</script>
---
+++ src/types/User.ts
@@ -0,0 +1,8 @@
+// src/types/User.ts
+
+export interface User {
+  id: number;
+  email: string;
+  name: string;
+  token: string;
+}
